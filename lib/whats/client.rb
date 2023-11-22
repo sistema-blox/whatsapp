ee0107fed6c29c244c429b287cb1b9fe680aca41
Raniery Sales <rany_sales@hotmail.com>
2023-11-22 12:03:41 -0300

    feat: update client to receive different content type and methods

--- lib/whats/client.rb
+++ lib/whats/client.rb
@@ -4,18 +4,25 @@ require "whats/actions/login"
 
 module Whats
   class Client
+    attr_reader :base_path, :token, :token_type, :payload, :content_type, :full_path
+
+    METHODS = [:get, :post]
+
     def initialize(token = nil, token_type = :bearer)
       @base_path = Whats.configuration.base_path
       @token = token || login.token
       @token_type = token_type
     end
 
-    def request(path, payload = nil)
-      full_path = "#{base_path}#{path}"
+    def request(path, payload = nil, content_type = "application/json", method = :post)
+      raise ArgumentError, 'Invalid method' unless METHODS.include?(method)
 
-      conn = Faraday.new(url: full_path, headers: headers)
-      response = conn.post { |request| request.body = body(payload) }
-      
+      @payload = payload
+      @content_type = content_type
+      @full_path = "#{base_path}#{path}"
+
+      response = method == :post ? post : get
+       
       body = JSON.parse(response.body)
 
       raise Whats::Errors::RequestError.new("API request error.", body) unless response.success?
@@ -25,7 +32,19 @@ module Whats
 
     private
 
-    attr_reader :base_path, :token, :token_type
+    def client
+      return @client if defined?(@client)
+
+      @client = Faraday.new(url: full_path, headers: headers)
+    end
+
+    def post
+      client.post { |req| req.body = body(payload) } 
+    end
+
+    def get
+      client.get
+    end
 
     def token_name
       case token_type
@@ -43,7 +62,7 @@ module Whats
     def headers
       {
         "Authorization" => "#{token_name} #{token}",
-        "Content-Type" => "application/json",
+        "Content-Type" => content_type,
       }
     end
 
