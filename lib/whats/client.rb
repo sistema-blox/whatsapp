900394101aae4cdc0719ff5b1768285a27071190
Raniery Sales <rany_sales@hotmail.com>
2023-11-23 10:56:36 -0300

    chore: remove memoize and add arg to overwrite token_type

--- lib/whats/client.rb
+++ lib/whats/client.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+# en_string_literal: true
 
 require "whats/actions/login"
 
@@ -7,6 +8,7 @@ module Whats
     attr_reader :base_path, :token, :token_type, :payload, :content_type, :full_path
 
     METHODS = [:get, :post]
+    AVAILABLE_TOKEN_TYPES = [:basic, :bearer, :oauth]
 
     def initialize(token = nil, token_type = :bearer)
       @base_path = Whats.configuration.base_path
@@ -14,13 +16,19 @@ module Whats
       @token_type = token_type
     end
 
-    def request(path, payload = nil, content_type = "application/json", method = :post)
+    def request(path, payload = nil, content_type = "application/json", method = :post, overwrite_token_type = nil)
       raise ArgumentError, 'Invalid method' unless METHODS.include?(method)
+      
+      unless overwrite_token_type.nil?
+        raise ArgumentError, 'Invalid token type' unless AVAILABLE_TOKEN_TYPES.include?(overwrite_token_type)
+
+        @token_type = overwrite_token_type
+      end
 
       @payload = payload
       @content_type = content_type
       @full_path = "#{base_path}#{path}"
-
+     
       response = method == :post ? post : get
        
       body = JSON.parse(response.body)
@@ -33,13 +41,13 @@ module Whats
     private
 
     def client
-      return @client if defined?(@client)
-
       @client = Faraday.new(url: full_path, headers: headers)
     end
 
     def post
-      client.post { |req| req.body = body(payload) } 
+      return client.post { |req| req.body = body(payload) } unless payload.nil?
+
+      client.post
     end
 
     def get
@@ -52,6 +60,8 @@ module Whats
         "Basic"
       when :bearer
         "Bearer"
+      when :oauth
+        "OAuth"
       end
     end
 
@@ -67,6 +77,8 @@ module Whats
     end
 
     def body(payload)
+      return payload if content_type.include?("image/png")
+      
       payload && payload.to_json
     end
   end
