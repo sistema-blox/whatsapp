54a2d0dbdce31c6be7998341433a4e8425f7c3f6
saleszera <raniery@saleszera.com>
2024-06-11 11:11:50 -0300

    feat: add get action to business profile

--- lib/whats/actions/business_profile.rb
+++ lib/whats/actions/business_profile.rb
@@ -4,36 +4,56 @@ require "whats/services/app/retrieve_media"
 module Whats
   module Actions
     class BusinessProfile
-      attr_reader :client, :from_phone_number_id, :path, :payload
+      attr_reader :client, :from_phone_number_id, :path, :payload, :action
 
       ENDPOINT = "/v18.0/%{from_phone_number_id}/whatsapp_business_profile"
       ACCEPTED_PARAMS = [:websites, :address, :description, :email, :about, :profile_picture_handle]
       HTTP_REGEX = /^http(s){0,1}:(\/){2}/
       EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+      ACTIONS = [:update, :get]
 
-      def initialize(client:, from_phone_number_id:, payload:)
+      def initialize(client:, from_phone_number_id:, payload: nil, action: :update)
         @client = client
         @from_phone_number_id = from_phone_number_id
         @path = URI::DEFAULT_PARSER.escape(ENDPOINT % { from_phone_number_id: from_phone_number_id })
-        @payload = payload
+        @payload = generate_payload(payload) if action == :update
+        @action = action
       end
 
       def call
-        client.request(path: path, payload: req_payload)
+        raise ArgumentError, "Unsupported action: #{action}" unless ACTIONS.include?(action)
+
+        send("#{action}_business_profile")
       end
 
       private
 
-      def req_payload
-        raise ArgumentError, "req_payload must be a Hash" unless payload.instance_of?(Hash)
+      def path
 
+      end
+
+      def get_business_profile
+        perform_request!(method: :get)
+      end
+
+      def update_business_profile
         set_profile_picture_url if payload[:file].instance_of?(File)
 
-        validate_parameters
+        perform_request!(payload:)
+      end
+
+      def perform_request!(aditional_params = {})
+        client.request(path:, **aditional_params)
+      end
+
+      def generate_payload(req_payload)
+        raise ArgumentError, "req_payload must be a Hash" unless req_payload.instance_of?(Hash)
+
+        validate_parameters(req_payload)
 
         {
           messaging_product: "whatsapp",
-        }.merge(payload)
+        }.merge(req_payload)
       end
 
       def set_profile_picture_url
@@ -42,7 +62,7 @@ module Whats
         file_path = payload[:file].path
         file_name = file_path.split("/").last
 
-        us_response = App::UploadSession.call(client: client, file_length: file_length, file_type: file_type, file_name: file_name)
+        us_response = App::UploadSession.call(client:, file_length:, file_type:, file_name:)
 
         raise "Upload session response does not contain an id." if us_response["id"].nil?
 
@@ -55,13 +75,13 @@ module Whats
         payload[:profile_picture_handle] = upload_response["h"]
       end
 
-      def validate_parameters
-        raise NotImplementedError, "Unsupported parameter(s) in payload: #{payload.keys - ACCEPTED_PARAMS}" unless payload.keys.all? { |k| ACCEPTED_PARAMS.include?(k) }
+      def validate_parameters(req_payload)
+        raise NotImplementedError, "Unsupported parameter(s) in payload: #{req_payload.keys - ACCEPTED_PARAMS}" unless req_payload.keys.all? { |k| ACCEPTED_PARAMS.include?(k) }
 
-        websites = payload[:websites]
-        address = payload[:address] || ""
-        description = payload[:description] || ""
-        email = payload[:email] || ""
+        websites = req_payload[:websites]
+        address = req_payload[:address] || ""
+        description = req_payload[:description] || ""
+        email = req_payload[:email] || ""
 
         if websites.instance_of?(String)
           raise char_limit_error("Website", 256) if websites.size > 256
